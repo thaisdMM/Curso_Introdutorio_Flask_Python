ab20bcee3eb102dd559c817c4c4f725191fc545d
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-14 16:24:29 +0100

    created database sqlite

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 #Importação
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__) #instancia o caminho do flask
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+
+db = SQLAlchemy(app)
 
 #Definir uma rota raiz(página inicial da api) e a função que será executada ao requisitar
 
